9b4ceb428b0ffa8fdbc099c74f85017e460d752d
Rahiman13 <rahimanshaik13@gmail.com>
2025-07-01 18:15:24 +0530

    login

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
+import Layout from './components/Layout';
+import Landing from './pages/RegistrationPage.jsx';
+import Login from './pages/Login.jsx';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#311188',
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/register" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
